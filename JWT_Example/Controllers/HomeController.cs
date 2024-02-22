c7f4ecbc8b1076cb4b5723b3f18d8afb383d8552
Rafael Pina <rpina@nortridge.com>
2024-02-22 13:23:06 -0600

    Add new controller and remove default controller

---
+++ JWT_Example/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JWT_Example.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(DateTime.Now);
+        }
+    }
+}
